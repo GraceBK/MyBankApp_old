053341b49e2425d453e4fa2197de409a6bc51bef
GraceBK <iris.b1991@gmail.com>
2019-01-10 06:00:03 +0100

    Initial commit

---
+++ app/src/main/java/fr/mbds/bankapp/LoginActivity.java
@@ -0,0 +1,19 @@
+/*
+ * Created by Grace BK on 1/10/19 5:49 AM
+ *
+ * Copyright (c) 2019. School project.
+ */
+
+package fr.mbds.bankapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
